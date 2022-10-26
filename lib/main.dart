13ecb3dfbd4b6849cfcf8518c533fbe433893a5e
lakshmankashyap <lkashyap92@gmail.com>
2022-10-26 10:39:22 +0530

    debug add

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,30 @@
 import 'package:flutter/material.dart';
-import './product_manger.dart';
+// import 'package:flutter/rendering.dart';
+
+import './product_manager.dart';
 
 void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> products = ['Food Tester '];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowMaterialGrid: true,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          brightness: Brightness.light,
+          primarySwatch: Colors.deepOrange,
+          accentColor: Colors.deepPurple),
       home: Scaffold(
-          appBar: AppBar(title: Text('EastList')), body: ProductManager()),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(startingProduct: 'Food Tester'),
+      ),
     );
   }
 }
--- lib/products.dart
+++ lib/products.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products);
+  Products([this.products = const []]) {
+    print('[Products Widget] Constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('[Products Widget] build()');
     return Column(
       children: products
           .map(
-            (e) => Card(
-              child: Column(
-                children: <Widget>[Image.asset('assets/test.jpg'), Text(e)],
-              ),
-            ),
+            (element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(element)
+                    ],
+                  ),
+                ),
           )
           .toList(),
     );
